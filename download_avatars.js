0d6d93cdaf4c2e9727e62d0563e12edd50c302a5
Ali Abji <aliabji@gmail.com>
2017-09-11 21:06:09 +0000

    github api request working

--- download_avatars.js
+++ download_avatars.js
@@ -7,12 +7,25 @@ var GITHUB_USER = 'aliabji';
 var GITHUB_TOKEN = '461ea187e96fab4f3d71ef416665556148a60a86';
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var buffer = ""
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  }
   
-  console.log(requestURL)
+  request.get(options, function(err, response, body) {
+    
+    if (err) throw err;
+    
+    if (body) {
+      console.log(body)
+    }
+    
+  })
 }
-
-
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
