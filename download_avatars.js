f1b086941fb1a97f228dfb02eafee34f930def5c
Ali Abji <aliabji@gmail.com>
2017-09-11 18:38:30 +0000

    github token working

--- download_avatars.js
+++ download_avatars.js
@@ -2,3 +2,18 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+
+var GITHUB_USER = 'aliabji';
+var GITHUB_TOKEN = '461ea187e96fab4f3d71ef416665556148a60a86';
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  
+  console.log(requestURL)
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
